159c0ec179a9b3c717f50dd1cca941f77df5ed02
joshcutler <josh.cutler@gmail.com>
2011-11-21 16:45:34 -0600

    Initial commit

---
+++ HW1.py
@@ -0,0 +1,14 @@
+def shout(txt):
+  return txt
+  
+def reverse(txt):
+  return txt
+  
+def reversewords(txt):
+  return txt
+  
+def reversewordletters(txt):
+  return txt
+  
+def piglatin(txt):
+  return txt
