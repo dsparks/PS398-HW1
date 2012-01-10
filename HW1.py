de0dec4277853b786178037316aa3151049bf1b6
Josh Cutler <josh.cutler@gmail.com>
2012-01-09 18:31:00 -0600

    Got an implementation of reversewords

--- HW1.py
+++ HW1.py
@@ -7,12 +7,47 @@ def shout(txt):
   return new_txt
   
 def reverse(txt):
-  return txt
+  if isinstance(txt, str) == False:
+    return ""
+      
+  return txt[::-1]
   
 def reversewords(txt):
-  return txt
+  if isinstance(txt, str) == False:
+    return ""
+  
+  new_text = ""
+  reversed_sentences = []
+    
+  tmp = txt.replace("?", ".")
+  tmp = tmp.replace("!", ".")
+  sentences = tmp.split(". ")
+  sentences = [s.strip() for s in sentences if len(s.strip()) > 0]
+  
+  last_sentence = sentences[len(sentences) - 1]
+  if last_sentence[len(last_sentence) - 1] == ".":
+    sentences[len(sentences) - 1] = last_sentence[0:len(last_sentence)-1]
+  
+  for sentence in sentences:
+    words = sentence.split()
+    words.reverse()
+    reversed_sentence = ""
+    for word in words:
+      reversed_sentence += word
+      reversed_sentence += " "
+    reversed_sentences.append(reversed_sentence[0:(len(reversed_sentence)-1)])
+  
+  for sentence in reversed_sentences:
+    if len(sentence) > 0:
+      new_text += sentence
+      new_text += ". "
+    
+  return new_text
   
 def reversewordletters(txt):
+  if isinstance(txt, str) == False:
+    return ""
+    
   return txt
   
 def piglatin(txt):
